3cd23f12c9ce4a5aebd8cb685d0253783a1e05bf
dmodalek <dominic@modalek.ch>
2017-03-28 22:11:53 +0200

    Add babel flow plugin

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -18,7 +18,6 @@ var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 var WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
 var getClientEnvironment = require('./env');
 var paths = require('./paths');
-var FlowReactProptypesPlugin = require('babel-plugin-flow-react-proptypes');
 
 // @remove-on-eject-begin
 // `path` is not used after eject - see https://github.com/facebookincubator/create-react-app/issues/1174
@@ -206,7 +205,6 @@ module.exports = {
     ];
   },
   plugins: [
-    new FlowReactProptypesPlugin(),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
